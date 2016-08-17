d1aae6f165b1b2e13aac8ef0cbc5c9825c33a436
bash <bas.heere@trifork.nl>
2016-08-17 15:34:56 +0200

    tussentijdse commit

--- src/main/java/com/bas/mvc/controllers/HomeController.java
+++ src/main/java/com/bas/mvc/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package com.bas.mvc.controllers;
 
+import com.bas.mvc.data.entities.Project;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,12 +12,17 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * Created by bas on 9-8-16.
  */
 @Controller
-@RequestMapping("/")
 public class HomeController {
 
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String goHome() {
+    @RequestMapping("/")
+    public String goHome(Model model) {
+        Project project = new Project();
+        project.setName("First project");
+        project.setSponsor("Nasa");
+        project.setDescription("description nasa");
+
+        model.addAttribute("currentProject",project);
         return "home";
     }
 
